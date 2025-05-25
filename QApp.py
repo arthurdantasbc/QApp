d22dd156a370061682a8bdefb75f193e81bc767c
VMilena30 <vmilena3010@gmail.com>
2025-05-25 02:03:34 -0300

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -486,7 +486,7 @@ def main():
         )
     
         # Botões centralizados horizontalmente
-        col1, col2, col3, col4 = st.columns([2, 1, 1, 2])
+        col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
         
         with col2:
             if st.button("English"):
