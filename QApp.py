0fb588c4a3cba642c99c0b2f8b62a51683b1332a
VMilena30 <vmilena3010@gmail.com>
2025-05-25 01:13:05 -0300

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -465,9 +465,10 @@ def main():
     
         # Caixa de informação sobre idioma
         st.info(
-            "ℹ️ For a better experience, you can change the language anytime during navigation."\n
-            "ℹ️ Para uma melhor experiência, você pode alterar o idioma a qualquer momento durante a navegação.\n" 
+            "ℹ️ For a better experience, you can change the language anytime during navigation.\n"
+            "ℹ️ Para uma melhor experiência, você pode alterar o idioma a qualquer momento durante a navegação."
         )
+
     
         st.stop()
     
